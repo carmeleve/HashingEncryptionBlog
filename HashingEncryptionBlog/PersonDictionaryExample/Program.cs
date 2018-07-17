44a53d484f9b691ad0d885a8154911d86412c324
Carmel Eve <Carmel.Eve@endjin.com>
2018-07-17 11:59:05 +0100

    Added console app for creating a person dictionary

---
+++ HashingEncryptionBlog/PersonDictionaryExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingEncryptionBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personDictionary = new Dictionary<string, Person>();
+            personDictionary.Add("Carmel", new Person("Carmel", "Eve"));
+        }
+
+        private class Person
+        {
+            private string firstName;
+            private string lastName;
+
+            public Person(string firstName, string lastName)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+        }
+    }
+}
